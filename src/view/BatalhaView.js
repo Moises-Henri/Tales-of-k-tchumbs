10905aaf00fdc6e0a681e7bc2fa2ec497cfe1c65
Moises-Henri <moisessouza4699@gmail.com>
2026-09-24 11:39:40 -0300

    Health Bar Add

--- src/view/BatalhaView.js
+++ src/view/BatalhaView.js
@@ -5,26 +5,43 @@ class BatalhaView {
                 ? `<p><strong>Mana:</strong> ${heroi.mana}</p>`
                 : "";
 
-        elemento.innerHTML = `
-            <h2>${titulo}</h2>
-            <h3>${heroi.nome}</h3>
+        const damage = heroi.vidaMaxima - heroi.vida;
+        const step = heroi.vidaMaxima / 5;
+        let imagemVida = '';
 
-            <p><strong>Raça:</strong> ${heroi.raca}</p>
-            <p><strong>Stamina:</strong> ${heroi.stamina}</p>
-            ${mana}
-            <p><strong>Nível/XP:</strong> ${heroi.nivel}</p>
-            <p><strong>Ataque:</strong> ${heroi.poderAtaque}</p>
-            <p><strong>Defesa:</strong> ${heroi.poderDefesa}</p>
-            <p><strong>Habilidade:</strong> ${heroi.habilidade}</p>
-            <p>
-            <strong>Vida:</strong>
-            <span class="valor-vida">
-                if((${heroi.vida}/${heroi.vidaMaxima}) == ${heroi.vidaMaxima}/5){
-                    <div id="barraVida"></div>
-                }
-            </span>
-        </p>
-        `;
+        if (damage <= step * 1) {
+            imagemVida = 'url(./assets/images/vidaCheia.png)';
+        } else if (damage <= step * 2) {
+            imagemVida = 'url(./assets/images/vidaComBarra-1.png)';
+        } else if (damage <= step * 3) {
+            imagemVida = 'url(./assets/images/vidaComBarra-2.png)';
+        } else if (damage <= step * 4) {
+            imagemVida = 'url(./assets/images/vidaComBarra-3.png)';
+        } else if (heroi.vidaMaxima == heroi.vida) {
+            imagemVida = 'url(./assets/images/vidaComBarra-4.png)';
+        } else {
+            imagemVida = 'url(./assets/images/vidaCOm0.png)'; 
+        }       
+
+
+elemento.innerHTML = `
+    <h2>${titulo}</h2>
+    <h3>${heroi.nome}</h3>
+
+    <p><strong>Raça:</strong> ${heroi.raca}</p>
+    <p><strong>Stamina:</strong> ${heroi.stamina}</p>
+    ${mana}
+    <p><strong>Nível/XP:</strong> ${heroi.nivel}</p>
+    <p><strong>Ataque:</strong> ${heroi.poderAtaque}</p>
+    <p><strong>Defesa:</strong> ${heroi.poderDefesa}</p>
+    <p><strong>Habilidade:</strong> ${heroi.habilidade}</p>
+    <p>
+        <strong>Vida:</strong>
+        <span class="valor-vida">
+            <div id="barraVida" style="background-image: ${imagemVida};"></div>
+        </span>
+    </p>
+`;
     }
 
     static atualizarPersonagens(jogador, adversario) {
